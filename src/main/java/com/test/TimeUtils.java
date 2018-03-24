c5c4b12d529f056c899df6cab43757e93370efe0
kjagadeeshkumar <kjagadeesh206@gmail.com>
2018-03-24 14:23:48 +0530

    test commit

--- src/main/java/com/test/TimeUtils.java
+++ src/main/java/com/test/TimeUtils.java
@@ -54,28 +54,8 @@ public class TimeUtils {
     
     public static void main(String args[]){
     	
-    	String dateStart = "11/03/14 09:29:58";
-        String dateStop = "11/03/14 09:33:43";
-
-       
-        SimpleDateFormat format = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
-
-        Date d1 = null;
-        Date d2 = null;
-        try {
-            d1 = format.parse(dateStart);
-            d2 = format.parse(dateStop);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        // Get msec from each, and subtract.
-        long diff = d2.getTime() - d1.getTime();
-        long diffSeconds = diff / 1000 % 60;
-        long diffMinutes = diff / (60 * 1000) % 60;
-        long diffHours = diff / (60 * 60 * 1000);
-        System.out.println("Time in seconds: " + diffSeconds + " seconds.");
-        System.out.println("Time in minutes: " + diffMinutes + " minutes.");
-        System.out.println("Time in hours: " + diffHours + " hours.");
+    	
     }
+	public void tester(){
+		System.out.println("tester");
 }
